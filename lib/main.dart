77bf1ecf4a0de8a9602ab1985e4cab834534a987
Lasenko Pavel <pal.2323zet@gmail.com>
2023-11-30 07:45:09 +0200

    Autoroute, arb

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,37 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter_showcase_2/core/setap_dependencies.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_showcase_2/presentation/router/router.dart';
 
 void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
   SetupDependenciesImp().setupDependencies();
-  runApp(const MyApp());
-//  var test = await GeocoderRepImp().getCoutryCode();
-
-  // final Either<Failure, NewsArticle> newsEither =
-  //     await NewsRepositoriesImp().getNews(country: 'us');
-
-  // newsEither.fold(
-  //   (failure) {
-  //     // Обработка ошибки
-  //   },
-  //   (news) {```
-  //     // Обработка успешного результата
-  //     for (Article article in news.articles) {
-  //       // print("Source Name: ${article.source.name}");
-  //       // print("Author: ${article.author ?? 'N/A'}");
-  //       // print("Title: ${article.title}");
-  //       // print("Description: ${article.description ?? 'N/A'}");
-  //       // print("URL: ${article.url}");
-  //       // print("URL to Image: ${article.urlToImage ?? 'N/A'}");
-  //       // print("Published At: ${article.publishedAt}");
-  //       // print("Content: ${article.content ?? 'N/A'}");
-  //       // print("----------------------");
-  //     }
-  //   },
-  // );
-  // var loc = await LocationRepositoryImp().getLocation();
-  // print('Location: ${loc.latitude}, ${loc.longitude}');
-  // var test = await GeocoderDataSource().getCountryByCoordinates();
-  // print(test);
-  // print(test.runtimeType);
-
-  // Either<Failure, WeatherModel> ewew = await WeatherRepImp().getWeather();
-  // ewew.fold((failure) => print(failure), (weather) => print(weather.current));
-
-  // var test = await NewsDataSoursImp().getNews(country: 'us');
-  // test.fold((failure) => print(failure), (news) => print(news.data));
-  // print(test.runtimeType);
-
-  // Either<Failure, NewsArticleModel> test =
-  //     await NewsRepositoriesImp().getNews();
-  // test.fold((failure) => print(failure), (news) {
-  //   for (Article article in news.articles) {
-  //     print("Source Name: ${article.source.name}");
-  //     print("Author: ${article.author ?? 'N/A'}");
-  //     print("Title: ${article.title}");
-  //     print("Description: ${article.description ?? 'N/A'}");
-  //     print("URL: ${article.url}");
-  //     print("URL to Image: ${article.urlToImage ?? 'N/A'}");
-  //     print("Published At: ${article.publishedAt}");
-  //     print("Content: ${article.content ?? 'N/A'}");
-  //     print("----------------------");
-  //   }
-  // });
-
-  // Either<Failure, NewsArticleModel> test =
-  //     await NewsRepositoriesImp().getSearchNews();
-  // test.fold((failure) => print(failure), (news) {
-  //   for (Article article in news.articles) {
-  //     print("Source Name: ${article.title}");
-  //   }
-  // });
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return AdaptiveTheme(
+      light: ThemeData(
+          primaryColor: Colors.blue, scaffoldBackgroundColor: Colors.white),
+      dark: ThemeData(primaryColor: Colors.blueGrey),
+      initial: AdaptiveThemeMode.light,
+      builder: (ThemeData light, ThemeData dark) => MaterialApp.router(
+        routerConfig: _appRouter.config(
+          navigatorObservers: () => [AutoRouterObserver()],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/presentation/router/router.dart
@@ -0,0 +1,46 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_showcase_2/presentation/pages/home_page.dart';
+import 'package:flutter_showcase_2/presentation/pages/onboarding.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  static const String firstRunKey = 'firstRun';
+
+  bool isFirstRun = true;
+
+  AppRouter() {
+    checkFirstRun();
+  }
+
+  Future<void> checkFirstRun() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // Получаем значение флага первого запуска
+    bool isFirstRun = prefs.getBool(firstRunKey) ?? true;
+
+    // Обновляем состояние флага
+    this.isFirstRun = isFirstRun;
+
+    // Если это первый запуск, отображаем Onboarding и сохраняем флаг
+    if (isFirstRun) {
+      prefs.setBool(firstRunKey, false);
+      notifyListeners(); // Обновляем маршруты после изменения состояния
+    }
+  }
+
+  @override
+  List<AutoRoute> get routes => [
+        if (isFirstRun)
+          AutoRoute(page: OnboardingRoute.page, path: '/', children: [
+            AutoRoute(page: HomeRoute.page, path: 'home'),
+          ]),
+        if (!isFirstRun)
+          AutoRoute(page: HomeRoute.page, path: '/', children: []),
+      ];
+}
+// AutoRoute(page: OnboardingRoute.page, path: '/', children: [
+//           AutoRoute(page: HomeRoute.page), /*...*/
+//         ]),
